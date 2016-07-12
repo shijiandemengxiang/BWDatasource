e1e04ad1c800f03f3964361fdf34ec5b96cc0c92
wangbin <wangb@rihuisoft.com>
2016-07-12 10:32:33 +0800

    Initial Commit

---
+++ BWSideMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BWSideMenu
+//
+//  Created by user on 16/7/12.
+//  Copyright © 2016年 rihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
